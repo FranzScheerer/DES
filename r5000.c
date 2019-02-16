a3b579ca3fcb446b3d1edeb94e3d44e671a9f6bd
Franz Scheerer <scheerer.software@gmail.com>
2019-02-16 14:29:59 +0100

    5000 bytes in reverse order
    knoppix@Microknoppix:~$ python v.py r5000.c
    Public key: X:  77549892854616986871765117333180964191285967669765
    Public key: Y:  134416075266886993913543428512851174079799271560366
    Sigature    X:  41283538043054766764335077349569556076585432689329
    Sigature    y:  74196036788895152141520642747766771438389782241061
    
    The verification of signature  False
    The verification of ecdsa signature  True

---
+++ r5000.c
@@ -0,0 +1,56 @@
+/********************
+https://www.includehelp.com/c-programs/c-program-to-print-contents-in-reverse-order-of-a-file-like-tac-command-in-linux.aspx?fbclid=IwAR2XeFJE8LtT5QF9-wS4BX6kyUZuG4ooARWyvhAZNC3yuVzCByyJBRSQP4Q
+Home » C solved programs
+ » C file handling programs
+
+C program to print contents in reverse order of a file (just like TAC command in Linux)
+This program will print the contents of given file name in reverse order. Just like tac command in Linux. tac command displays contents of the file in reverse order. Same as tac command in this program you have to supply file name.
+The syntax of executing this program
+./program-name file-name
+
+*/
+//Write a program to print reverse content of file
+#include <stdio.h>
+#include <string.h>
+ 
+int main(int argc, char *argv[])
+{
+    FILE *fp1;
+    char buf [5000]; 
+    int c, cnt = 0;
+    int i   = 0;
+     
+    if( argc != 1 )
+    {
+        printf("Wrong number of arguments!!!\n");
+        printf("Please use \"program\" without command line parameters\n");
+        return -1;
+    }
+     
+    fp1 = stdin;
+    if( fp1 == NULL )
+    {
+        printf("\n%s Stdin can not be used : \n");
+        return -1;
+    }
+     
+    cnt = 0; 
+    while( (c = fgetc(fp1)) != -1 )
+    {
+        buf[cnt] = c;
+        cnt++;
+        if (cnt == sizeof(buf)){
+           while (cnt > 0){
+              cnt--;
+              fputc(buf[cnt], stdout);
+           }
+        }
+    }
+    while (cnt > 0){
+       cnt--;
+       fputc(buf[cnt], stdout);
+    }
+
+    fprintf(stderr,"Finished\n");
+}
+     

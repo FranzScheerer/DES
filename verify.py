b98bf2fe5ae4d13f84d2763a480dbbf8915bc825
Franz Scheerer <scheerer.software@gmail.com>
2019-03-15 16:16:56 +0100

    a = 0, b = 0, p = RSA100_2, n = p, G = (1,1)
    Base point:
    [1, 1]
    Second point
    [14831045122634000683314923472625911205124474303609L, 23160841639538065978351404137863800917397980715582L]
    test Schnoor  True
    s  [11153326707006480327816386591236714299106725892032L, 29081908087352132695982037498474752661721987767380L]
    y  [10370639532689708476791829890643858894134107003112L, 693943244493541975439355284117379359832968997972L]
    knoppix@Microknoppix:~$ python verify.py verify.py
    test Schnoor  True
    s  [11153326707006480327816386591236714299106725892032L, 29081908087352132695982037498474752661721987767380L]
    y  [10370639532689708476791829890643858894134107003112L, 693943244493541975439355284117379359832968997972L]

--- verify.py
+++ verify.py
@@ -1,29 +1,32 @@
-import math, hashlib, sys
+import sys, math, hashlib, random, time
 
+def writeNumber(number, fnam):
+  f = open(fnam, 'wb')
+  n = number
+  while n > 0:
+    byte = n % 256
+    n = n / 256
+    f.write(chr(byte))
+  f.close()
+ 
 def readNumber(fnam):
   f = open(fnam, 'rb')
-  n = 0
-  snum = f.read()
-  for i in range(len(snum)):
-    n = (n << 8) ^ ord(snum[len(snum)-i-1])   
+  txt = f.read()
   f.close()
-  return n
-
-def hextxt2num(x):
-  res = 0
-  for c in x:
-    if ord(c) < 58 and ord(c) >= 48:
-       res = (res<<4) + ord(c) - 48
-    elif ord(c) <= ord('F') and ord(c) >= ord('A'):
-       res = (res<<4) + ord(c) - 55
-  return res
-
-prime = 1393796574908163946345982392040522594173643
-h_ = 12
-n = (prime + 1)/(h_)
-b = 1689398
+  number = 0 
+  for c in reversed(txt):
+    number = (number << 8) + ord(c)
+  return number
+ 
+# VERIFY SCHNORR
+prime = 40094690950920881030683735292761468389214899724061
+#
+# A prime factor of RSA100
+#
+h = 1
+n = prime
+b = 0
 a = 0
-
 
 def inv(b,m):
   s = 0
@@ -31,7 +34,7 @@ def inv(b,m):
   a = m
   while b != 1:
     q = a/b
-    aa = b 
+    aa = b
     b = a % b
     a = aa
     ss = t
@@ -41,6 +44,7 @@ def inv(b,m):
     t = t + m
   return t
 
+
 def h(x):
   dx1 = hashlib.sha256(x).digest()
   res = 0
@@ -48,22 +52,16 @@ def h(x):
     res = (res<<8) ^ ord(cx)
   return res % n
 
-def genP(x,a,b):
-   while (pow(x**3 + a*x + b, (prime - 1) / 2, prime) != 1):
-     x = x + 1
-   y = pow(x**3 + a*x + b, (prime + 1) / 4, prime)
-   return [(x) % prime, (y) % prime]
-
 def addP(P,Q):
   x1 = P[0]
   x2 = Q[0]
   y1 = P[1] 
   y2 = Q[1] 
-  while x1 < x2:
-     x1 = x1 + prime
+  if x2 > x1:
+    x2 = x2 - prime
   if x1 == x2:
      s = ((3*(x1**2) + a) * inv(2*y1, prime)) % prime
-  else:  
+  else:
      s = ((y1-y2) * inv(x1-x2, prime)) % prime
   xr = s**2 - x1 - x2
   yr = s * (x1-xr) - y1 
@@ -77,42 +75,27 @@ def mulP(P,n):
     n = (-1)*n 
   PP = resP
   while n > 0:
-     if (n % 2 != 0):   
+     if (n & 1) != 0:   
          if isFirst:
             resP = PP
             isFirst = False
          else:
             resP = addP(resP,PP)
-     PP = addP(PP, PP) 
-     n = n / 2
+     PP = addP(PP, PP)
+     n = n >> 1 
   return resP
 
-def verify(G,s,Y,e,m):
-  return e == h(str(addP(mulP(G,s),mulP(Y,e))[0]) + m)
+P = [1,1]
 
-def ecdsa_v(G,m,S,Y):
-  si = inv(S[0], n)
-  hh = h(m + str(S[1]))
-  u1 = (si * hh) % n
-  u2 = (si * S[1]) % n
-  return addP( mulP(G, u1), mulP(Y, u2) )[0] == S[1]
-
-
-P = genP(17,a,b)
-P = mulP(P,h_)
-
-f = open(sys.argv[1], 'r')
+f = open(sys.argv[1],'r')
 message = f.read()
 f.close()
 
-y = [readNumber('y0'), readNumber('y1')]
+
 sig = [readNumber('s0'), readNumber('s1')]
+y = [readNumber('y0'), readNumber('y1')]
 
-print "Public key: X: ", y[0] % prime
-print "Public key: Y: ", y[1] % prime
-print "Sigature    X: ", sig[0]
-print "Sigature    y: ", sig[1]
-print ""
-print "The verification of signature ", verify(P, sig[0], y, sig[1], message)
-print "The verification of ecdsa signature ", ecdsa_v(P,message,sig, y)
+print "test Schnoor ", h(str(addP(mulP(P,sig[0]),mulP(y,sig[1]))[0]) + message) == sig[1]
 
+print "s ", sig
+print "y ", y

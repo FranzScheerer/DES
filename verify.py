896d4196993b1a1111b7419b2aff206b0ebd869e
Franz Scheerer <scheerer.software@gmail.com>
2019-03-16 16:21:27 +0100

    2^127 - 1 is a little small - but ok
    knoppix@Microknoppix:~$ python sign.py verify.py
    Base point:
    [1, 1]
    Second point
    [3581891278078139078933828346565509249L, 11647514514550228081622714091315760744L]
    test Schnoor  True
    s  [166058839010907233527415434464316584324L, 21559045905086179602707502286896938072L]
    y  [148980044462237090162008666269753823323L, 3930784182658308025037582648976390834L]
    knoppix@Microknoppix:~$ python verify.py verify.py
    test Schnoor  True
    Signature  166058839010907233527415434464316584324
    signature  21559045905086179602707502286896938072
    Public Key  148980044462237090162008666269753823323
    Public Key  3930784182658308025037582648976390834

--- verify.py
+++ verify.py
@@ -19,50 +19,27 @@ def readNumber(fnam):
   return number
  
 # VERIFY SCHNORR
-prime = 40094690950920881030683735292761468389214899724061
-#
-# A prime factor of RSA100
-#
-h = 1
+prime = 2**127 - 1
 n = prime
 b = 0
 a = 0
 
-def inv(b,m):
-  s = 0
-  t = 1
-  a = m
-  while b != 1:
-    q = a/b
-    aa = b
-    b = a % b
-    a = aa
-    ss = t
-    t = s - q*t
-    s = ss
-  if t < 0:
-    t = t + m
-  return t
-
-
 def h(x):
-  dx1 = hashlib.sha256(x).digest()
+  dx = hashlib.sha256(x).digest()
   res = 0
-  for cx in (dx1):
+  for cx in dx:
     res = (res<<8) ^ ord(cx)
-  return res % n
+  return res % prime
 
 def addP(P,Q):
   x1 = P[0]
   x2 = Q[0]
   y1 = P[1] 
   y2 = Q[1] 
-  if x2 > x1:
-    x2 = x2 - prime
   if x1 == x2:
-     s = ((3*(x1**2) + a) * inv(2*y1, prime)) % prime
+     s = ((3*(x1**2) + a) * pow(2*y1, prime-2, prime)) % prime
   else:
-     s = ((y1-y2) * inv(x1-x2, prime)) % prime
+     s = ((y1-y2) * pow(x1-x2, prime-2, prime)) % prime
   xr = s**2 - x1 - x2
   yr = s * (x1-xr) - y1 
   return [xr % prime, yr % prime]
@@ -97,5 +74,7 @@ y = [readNumber('y0'), readNumber('y1')]
 
 print "test Schnoor ", h(str(addP(mulP(P,sig[0]),mulP(y,sig[1]))[0]) + message) == sig[1]
 
-print "s ", sig
-print "y ", y
+print "Signature ", sig[0]
+print "signature ", sig[1]
+print "Public Key ", y[0]
+print "Public Key ", y[1]

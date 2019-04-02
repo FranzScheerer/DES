9325df3c28a50c8e8c6f5206036e9331a4ace77a
Franz Scheerer <scheerer.software@gmail.com>
2019-04-02 18:05:01 +0200

    Why not just this curve
    knoppix@Microknoppix:~$ python nist.py nist.py
    Public key: X:  87208187038084712473259243319204152907099626105179826564143777103141608068097
    Public key: Y:  80964613367030991789634019645338653438735304518340029718681440152044712794746
    Sigature    X:  9951690872273003605203525411751543368647408252431509272060846414493564347708
    Sigature    y:  115011685257614479319438099765096827320440374267665937367463897052714577369401
    
    The verification of signature  True
    The verification of ecdsa signature  False

--- nist.py
+++ nist.py
@@ -1,72 +1,31 @@
-import sys, math, hashlib, random, time
+import random, math, hashlib, sys
+
+def readNumber(fnam):
+  f = open(fnam, 'rb')
+  n = 0
+  snum = f.read()
+  for i in range(len(snum)):
+    n = (n << 8) ^ ord(snum[len(snum)-i-1])   
+  f.close()
+  return n
 
 def hextxt2num(x):
   res = 0
   for c in x:
     if ord(c) < 58 and ord(c) >= 48:
        res = (res<<4) + ord(c) - 48
+    elif ord(c) <= ord('f') and ord(c) >= ord('a'):
+       res = (res<<4) + ord(c) - 87
     elif ord(c) <= ord('F') and ord(c) >= ord('A'):
        res = (res<<4) + ord(c) - 55
   return res
 
-prime = hextxt2num("FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE FFFFEE37") 
-a = 0
-b = 3
-n4 = hextxt2num("FFFFFFFF FFFFFFFF FFFFFFFE 26F2FC17 0F69466A 74DEFD8D")
-
-def writeNumber(number, fnam):
-  f = open(fnam, 'wb')
-  n = number
-  while n > 0:
-    byte = n % 256
-    n = n / 256
-    f.write(chr(byte))
-  f.close()
+#prime = (2**160 * 5 * 23) + 86427
+prime = 115792089210356248762697446949407573530086143415290314195533631308867097853951
+a = prime - 3
+b = hextxt2num("5ac635d8 aa3a93e7 b3ebbd55 769886bc 651d06b0 cc53b0f6 3bce3c3e27d2604b")
+n4 =  115792089210356248762697446949407573529996955224135760342422259061068512044369
  
-def bin2num(x):
-  res = 0
-  for c in x:
-    res = (res<<8) ^ ord(c)
-  return res
-
-def num2bin(x):
-  res = ''
-  while x > 0:
-    res = chr(x % 256) + res
-    x /= 256
-  return res
-
-def gcd(a,b):
-  while b > 0:
-    a,b = b,a % b
-  return a
-
-def nextPrime(p):
- while p % 8 != 3:
-   p = p + 1
- return nextPrime_odd(p)
-
-def nextPrime_odd(p):
-  m_ = 3*5*7*11*13*17*19*23*29
-  while True:
-    while gcd(p,m_) != 1:
-      p = p + 8 
-    q = (p+1)/4
-    if (pow(2,p-1,p) != 1 or pow(2,q-1,q) != 1):
-       p = p + 8
-       continue
-    if (pow(3,p-1,p) != 1 or pow(3,q-1,q) != 1):
-       p = p + 8
-       continue
-    if (pow(5,p-1,p) != 1 or pow(5,q-1,q) != 1):
-       p = p + 8
-       continue
-    if (pow(17,p-1,p) != 1 or pow(17,q-1,q) != 1):
-       p = p + 8
-       continue
-    break
-  return p
-
 def inv(b,m):
   s = 0
   t = 1
@@ -83,46 +42,6 @@ def inv(b,m):
     t = t + m
   return t
 
-def E(m, key, prime):
-  return pow(m, key, prime)
-
-def D(c, key, prime):
-  key = inv(key, prime - 1)
-  return pow(c, key, prime)
-
-def code2num(x):
-  res = 0
-  for c in x:
-     if ord(c) >= 48 and ord(c) < 58:
-       res = (res << 6) + ord(c) - 48
-     if ord(c) >= 65 and ord(c) < 91:
-       res = (res << 6) + ord(c) - 55
-     if ord(c) >= 97 and ord(c) < 123:
-       res = (res << 6) + ord(c) - 61
-     if c == '#': 
-       res = (res << 6) + 62
-     if c == '/': 
-       res = (res << 6) + 63
-  return res
-
-def num2code(x):
-  res = ''
-  while x > 0:
-    y = x % 64
-    if y < 10:
-       res = chr( y + 48 ) + res
-    elif y < 36:
-       res = chr( y + 55 ) + res
-    elif y < 62:
-       res = chr( y + 61 ) + res 
-    elif y == 62:
-       res = '#' + res 
-    elif y == 63:
-       res = '/' + res 
-    x /= 64
-  return res
-
-
 def h(x):
   dx1 = hashlib.sha256(x).digest()
   res = 0
@@ -130,44 +49,22 @@ def h(x):
     res = (res<<8) ^ ord(cx)
   return res % n4
 
-def random256(m):
-  md = hashlib.sha256("***RANDOM-SEED_X***")
-  md.update('large key value for generation of random number')
-  md.update( m )
-  result = 0
-  largestr = md.digest()
-  for i in range(len(largestr)):
-      result = (result << 8) ^ ord(largestr[i])
-  return result
-
-def randomX(m):
-  md = hashlib.sha256("***RANDOM-SEED_X***")
-  md.update('large key value for generation of random number')
-  md.update( m )
-#  md.update( str(random.randint(0, 999999999999)) )
-  md.update( str(time.gmtime().tm_year + time.gmtime().tm_mday))
-  result = 0
-  largestr = md.digest()
-  for i in range(len(largestr)):
-      result = (result << 8) ^ ord(largestr[i])
-  return result
-
 def genP(x,a,b):
-   while (pow(x**3 + a*x + b, (prime - 1)/2, prime) != 1):
+   while pow(x**3 + a*x + b, (prime - 1)/2, prime) != 1:
      x = x + 1
    y = pow(x**3 + a*x + b, (prime + 1)/4, prime)
-   return [(x) % prime, (y) % prime]
+   return [x % prime, y % prime]
 
 def addP(P,Q):
   x1 = P[0]
   x2 = Q[0]
   y1 = P[1] 
   y2 = Q[1] 
-  if x2 > x1:
-    x2 = x2 - prime
+  while x1 < x2:
+     x1 = x1 + prime
   if x1 == x2:
      s = ((3*(x1**2) + a) * inv(2*y1, prime)) % prime
-  else:
+  else:  
      s = ((y1-y2) * inv(x1-x2, prime)) % prime
   xr = s**2 - x1 - x2
   yr = s * (x1-xr) - y1 
@@ -181,28 +78,18 @@ def mulP(P,n):
     n = (-1)*n 
   PP = resP
   while n > 0:
-     if (n & 1) != 0:   
+     if (n % 2 != 0):   
          if isFirst:
             resP = PP
             isFirst = False
          else:
             resP = addP(resP,PP)
-     PP = addP(PP, PP)
-     n = n >> 1 
+     PP = addP(PP, PP) 
+     n = n / 2
   return resP
 
-def signSchnorr(G,m,x):
-  k = randomX(m)
-  R = mulP(G,k)
-  e = h(str(R[0]) + m)
-  return [(k - x*e) % n4, e]
-
-def ecdsa(G,m,x):
-  k = randomX(m)
-  R = mulP(G,k)
-  hh = h(m+str(R[0]))
-  s = ( inv(k,n4) * (hh + R[0]*x) ) % n4
-  return [s, R[0]]
+def verify(G,s,Y,e,m):
+  return e == h(str(addP(mulP(G,s),mulP(Y,e))[0]) + m)
 
 def ecdsa_v(G,m,S,Y):
   si = inv(S[0], n4)
@@ -211,34 +98,44 @@ def ecdsa_v(G,m,S,Y):
   u2 = (si * S[1]) % n4
   return addP( mulP(G, u1), mulP(Y, u2) )[0] == S[1]
 
-
-# x-value of the starting point  
-x = 17
-
-# The starting point which is added many times to itself 
-#cinv = inv(c, prime)
-P = genP(x,a,b)
+P = genP((a-17), a, b)
 P = mulP(P,4)
 
-f = open(sys.argv[1],'r')
+f = open(sys.argv[1], 'r')
 message = f.read()
 f.close()
 
-x = 2*random256(sys.argv[1]) + 1
-y = mulP(P,x)
+y = [readNumber('y0'), readNumber('y1')]
+sig = [readNumber('s0'), readNumber('s1')]
 
-#print "The public key for Schnorr's signature \n", y
-writeNumber(y[0],'y0')
-writeNumber(y[1],'y1')
+print "Public key: X: ", y[0] % prime
+print "Public key: Y: ", y[1] % prime
+print "Sigature    X: ", sig[0]
+print "Sigature    y: ", sig[1]
+print ""
+print "The verification of signature ", verify(P, sig[0], y, sig[1], message)
+print "The verification of ecdsa signature ", ecdsa_v(P,message,sig, y)
 
-#sig = signSchnorr(P, message, x)
-sig = ecdsa(P, message, x)
-print "Verify: ", ecdsa_v(P,message,sig,y)
+def writeNumber(number, fnam):
+  f = open(fnam, 'wb')
+  n = number
+  while n > 0:
+    byte = n % 256
+    n = n / 256
+    f.write(chr(byte))
+  f.close()
 
+def signSchnorr(G,m,x):
+  k = h(m + 'kk1')
+  R = mulP(G,k)
+  e = h(str(R[0]) + m)
+  return [(k - x*e) % n4, e]
+
+hxx = h('kk1_' + str(777*random.random()))
+sig = signSchnorr(P, message, hxx)
+y = mulP(P, hxx)
 writeNumber(sig[0],'s0')
 writeNumber(sig[1],'s1')
-
-
-print "test Schnoor ", h(str(addP(mulP(P,sig[0]),mulP(y,sig[1]))[0]) + message) == sig[1]
-
-
+writeNumber(y[0],'y0')
+writeNumber(y[1],'y1')
+#print "Test n ", pow(7,n4-1,n4) == 1

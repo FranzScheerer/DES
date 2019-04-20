eb6055ba5c6b59049ad92579149808e0dfe63ffb
Franz Scheerer <scheerer.software@gmail.com>
2019-04-20 20:01:57 +0200

    The proof of hasse theorem
    knoppix@Microknoppix:~$ python ed.py hasseproof.py
    Public key: X:  4085333096699640207220766872157527703392232319330461589440
    Public key: Y:  79212668459156835574944356563322488352443579417662185479
    Sigature    X:  5765903018870386506846484282764631953883875004793207748474
    Sigature    y:  887775227039691123918600017462
    
    The verification of signature  True
    The verification of ecdsa signature   True
    
     TEST TEST TEST 
    prime        True
    prime order  True
    order        True

---
+++ hasseproof.py
@@ -0,0 +1,23 @@
+import math
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+for prime in primes:
+ print "prime ", prime
+ min = (prime + 1) - 2 * math.sqrt(prime)
+ max = (prime + 1) + 2 * math.sqrt(prime)
+
+ for a in range(prime):
+  for b in range(prime):
+    cnt = 1                   # point at infinity
+#   print "prime ", prime, "\n cnt = ", cnt, " b = ", b
+    for x in range(prime):
+      yy = x*x*x +(a)*x + b
+      if yy % prime == 0:
+         cnt = cnt + 1
+      else:
+         if pow(yy, (prime-1)/2, prime) == 1:
+           cnt = cnt + 2 
+    if cnt > max or cnt < min:
+      print "error b = ", b, " cnt = ", cnt
+

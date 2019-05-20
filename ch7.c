07b66d163b32aed0dd9df067a7ba59b3160f2431
Franz Scheerer <scheerer.software@gmail.com>
2019-05-20 17:25:06 +0200

    The perfect stream cipher with cipher feedback
    knoppix@Microknoppix:~$ cat ch7.c | ./a.out ch7.c > x
    h7: 1C021ED45C2AC427459F27AF38C58EB0D341EB8F2735216164053FE45BBE80B5
    k

---
+++ ch7.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+
+#define N 256
+#define ROUNDS 3
+
+int main(int argn, char **argv){
+  int s[N], s_[N];
+  int klen,t,i,j=0;
+  int i2,j2;
+  int r, c;
+  unsigned char cout;
+
+  if (argn < 2){
+    fprintf(stderr, "encrypt: cat file | ./a.out password > efile\n");
+    fprintf(stderr, "decrypt: cat efile | ./a.out password D > file\n");
+    return 0;
+  }
+  klen = strlen(argv[1]);
+  for (i=0; i<N; i++){
+      s[i] = (i + klen) % N;
+      s_[i] = i;
+  }
+  for (r=0; r < ROUNDS; r++)
+  {
+    for (i=0;i<N; i++){
+      j = (j + s[i] + argv[1][i % klen]) % N;
+      t = s[i] + s[j];
+      s[i] = t - s[i];
+      s[j] = t - s[j];
+    }
+  }
+  for (r=0; r < ROUNDS; r++)
+  {
+    for (i=0;i<N; i++){
+      j = (j + s_[i] + argv[1][(i+r) % klen]) % N;
+      t = s_[i] + s_[j];
+      s_[i] = t - s_[i];
+      s_[j] = t - s_[j];
+    }
+  }
+  i = s[0];
+  j = s[1];
+  i2 = s[2];
+  j2 = s[N-1];
+  while ((c = fgetc(stdin)) != -1){
+    i = (i + 1) % N;
+    j = (j + s[i]) % N;
+    i2 = (i2 + s[j]) % N;
+    j2 = (j2 + s[i2]) % N;
+    t = s_[i] + s_[j];
+    s_[i] = t - s_[i];
+    s_[j] = t - s_[j];
+    t = s[i2] + s[j2];
+    s[i2] = t - s[i2];
+    s[j2] = t - s[j2];
+    if (argn == 2) 
+       cout = (c + s_[s[t % N]]) % N;
+    else
+       cout = (N + c - s_[s[t % N]]) % N;
+    if (argn > 2) j ^= c; else j ^= cout;
+    printf("%c", cout);    
+  }
+  fprintf(stderr,"h7: ");
+  for (r=0;r<32;r++){
+    i = (i + 1) % N;
+    j = (j + s[i]) % N;
+    i2 = (i2 + s[j]) % N;
+    j2 = (j2 + s[i2]) % N;
+    t = s[i2] + s[j2];
+    s[i2] = t - s[i2];
+    s[j2] = t - s[j2];
+    cout = (c + s_[s[t % N]]) % N;
+    fprintf(stderr, "%02X", cout);    
+  }
+  fprintf(stderr, "\n");    
+}
+      

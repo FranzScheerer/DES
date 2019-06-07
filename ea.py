49d04876b51c4b00e645bfdd71eca9b16641987f
Franz Scheerer <scheerer.software@gmail.com>
2019-06-07 17:45:15 +0200

    The final curve/challenge
    A prime greater than 12 times 2^161 
    p =  35076039295941670036888435985190792471742381074011
    Public key: X:  1159638104883857522215823755355249838974911903805
    Public key: Y:  4243388743533139790973856800848144092143677866287
    Sigature    X:  1653768213998036406229019084741193880064759263735
    Sigature    y:  252822876981209659110806205838
    
    The verification of signature  True
    Bitlength  162
    
    Challenege: a = 0, b = p - 3, 
     p =  35076039295941670036888435985190792471742381074011 
     check prime     True
     check (p+1)/12  True
     check order     True
    
    
    Px  32894394290624598691836362120659170358991318288370
    Py  14149199586324817720990661223044872347197732297339
    
    
    Qx  17842297557050266910423191144677963782807320033418
    Qy  18282069625197418022600878607933465338868131192230
    
    
    Win 10,000 Dollars 
    Challenge: Find d, Q = d P

--- ea.py
+++ ea.py
@@ -41,25 +41,26 @@ def hextxt2num(x):
        res = (res<<4) + ord(c) - 55
   return res
 
-prime = nextPrime(2**128)
+prime = nextPrime(12 * 2**161)
+print "A prime greater than 12 times 2^161 \np = ", prime
 a = 0
 b = prime - 3
  
-n4 = (prime + 1)/12
+r = (prime + 1) / 12
 hsize = 2**100 + 7
  
-def inv(b,m):
+def inv(b,a):
+  m = a
   s = 0
   t = 1
-  a = m
   while b != 1:
     q = a/b
-    aa = b
+    a_tmp = b
     b = a % b
-    a = aa
-    ss = t
+    a = a_tmp
+    s_tmp = t
     t = s - q*t
-    s = ss
+    s = s_tmp
   if t < 0:
     t = t + m
   return t
@@ -76,11 +77,11 @@ def addP(P,Q):
   x2 = Q[0]
   y1 = P[1] 
   y2 = Q[1] 
-  while x1 < x2:
-     x1 = x1 + prime
   if x1 == x2:
      s = ((3*(x1**2) + a) * inv(2*y1, prime)) % prime
   else:  
+     if x1 < x2:
+        x1 = x1 + prime
      s = ((y1-y2) * inv(x1-x2, prime)) % prime
   xr = s**2 - x1 - x2
   yr = s * (x1-xr) - y1 
@@ -108,15 +109,15 @@ def verify(G,s,Y,e,m):
   return e == h(str(addP(mulP(G,s),mulP(Y,e))[0]) + m)
 
 def ecdsa_v(G,m,S,Y):
-  si = inv(S[0], n4)
+  si = inv(S[0], r)
   hh = h(m + str(S[1]))
-  u1 = (si * hh) % n4
-  u2 = (si * S[1]) % n4
+  u1 = (si * hh) % r
+  u2 = (si * S[1]) % r
   return addP( mulP(G, u1), mulP(Y, u2) )[0] == S[1]
 
 x = 1
-if pow(x**3 + a*x + b, (prime - 1)/2, prime) != 1:
-   x = prime - x
+while pow(x**3 + a*x + b, (prime - 1)/2, prime) != 1:
+   x = x + 1
 y = pow(x**3 + a*x + b, (prime + 1)/4, prime)
 P = [x % prime, y % prime]
 P = mulP(P, 12)
@@ -158,7 +159,7 @@ def signSchnorr(G,m,x):
   k = h(m + 'kk1')
   R = mulP(G,k)
   e = h(str(R[0]) + m)
-  return [(k - x*e) % n4, e]
+  return [(k - x*e) % r, e]
 
 hxx = h('kk1_' + str(777*random.random()))
 sig = signSchnorr(P, message, hxx)
@@ -169,24 +170,24 @@ writeNumber(y[0],'y0')
 writeNumber(y[1],'y1')
 
  
-lb = n4
+lb = r
 cx = 0
 while lb > 0:
   lb = lb/2
   cx = cx + 1
 print "Bitlength ", cx
-print "\nChallenege: a = 0, b = 3-p, \n p = ", prime, "\n check prime   ", pow(7,prime-1,prime)
-q = n4
-print " check (p+1)/12 ", pow(7,q-1,q)
-print " check order   ", mulP(P,q+1) == P
+print "\nChallenege: a = 0, b = p - 3, \n p = ", prime, "\n check prime    ", pow(7,prime-1,prime) == 1
+q = r
+print " check (p+1)/12 ", pow(7,q-1,q) == 1
+print " check order    ", mulP(P,q+1) == P
 print "\n\nPx ", P[0]
 print "Py ", P[1]
 
 Q = mulP(P, random.randint(2,prime-2) )
-
-print "Challenge: Find d, Q = d P "
 print "\n\nQx ", Q[0]
 print "Qy ", Q[1]
+print "\n\nWin 10,000 Dollars "
+print "Challenge: Find d, Q = d P "
 
 #prime = nextPrime(100)
 #print "\n prime = ", prime
@@ -198,11 +199,11 @@ print "Qy ", Q[1]
 #P = [x % prime, y % prime]
 #P = mulP(P, 12)
 
-#n4 = (prime + 1)/12
-#for i in range(n4/3):
+#r = (prime + 1)/12
+#for i in range(r/3):
 #  print mulP(P,3*i+1), mulP(P,3*i+2), mulP(P,3*i+3) 
 
 
-for x in range(100):
-  wa(mulP(Q,x+2)[1],'test') 
-  wa(mulP(Q,x+2)[0],'test') 
+#for x in range(100):
+#  wa(mulP(Q,x+2)[1],'test') 
+#  wa(mulP(Q,x+2)[0],'test') 

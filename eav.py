54dd079fc70cd135a96ed35c262907b2028e8710
Franz Scheerer <scheerer.software@gmail.com>
2019-08-25 16:49:18 +0200

    Sign and verify with elliptic curve
    knoppix@Microknoppix:~/DES-master$ python eav.py ea.py
    A prime greater than 12 times 2^131 
    p =  32667107224410092492483962313449748403011
    Public key: X:  392937783511856504522659672430734557979
    Public key: Y:  4034124688196261675086793643422882347425
    Sigature    X:  22240767701071001497689106451467643077
    Sigature    y:  2266241064326228681346827224289334142986
    
    The verification of signature  True

--- eav.py
+++ eav.py
@@ -1,5 +1,8 @@
 import random, math, hashlib, sys
 
+PublickeyX =  392937783511856504522659672430734557979
+PublickeyY =  4034124688196261675086793643422882347425
+
 def update_spritz():
     global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
     i_spritz = (i_spritz + w_spritz) % 256
@@ -41,24 +44,6 @@ def gcd(a,b):
     a,b = b,a % b
   return a
 
-#
-# p and (p+1)/12 are primes
-#
-def nextPrime(p):
- while p % 12 != 11:
-   p = p + 1
- return nextPrime_odd(p)
-
-def nextPrime_odd(p):
-  m_ =  5 * 7 * 11 * 13 * 17 * 19 * 23 * 29 * 31 * 37 * 41 * 43 * 47
-  while True:
-    while gcd(p, m_) != 1 or gcd((p+1)/12, m_) != 1:
-      p = p + 12 
-    if (pow(7,p-1,p) != 1 or pow(7, (p+1)/12 - 1, (p+1)/12) != 1):
-      p = p + 12
-      continue
-    return p
-
 def readNumber(fnam):
   f = open(fnam, 'rb')
   n = 0
@@ -68,18 +53,8 @@ def readNumber(fnam):
   f.close()
   return n
 
-def hextxt2num(x):
-  res = 0
-  for c in x:
-    if ord(c) < 58 and ord(c) >= 48:
-       res = (res<<4) + ord(c) - 48
-    elif ord(c) <= ord('f') and ord(c) >= ord('a'):
-       res = (res<<4) + ord(c) - 87
-    elif ord(c) <= ord('F') and ord(c) >= ord('A'):
-       res = (res<<4) + ord(c) - 55
-  return res
 
-prime = nextPrime(12 * 2**131)
+prime = 32667107224410092492483962313449748403011
 
 print "A prime greater than 12 times 2^131 \np = ", prime
 
@@ -159,12 +134,6 @@ def mulP(P,n):
 def verify(G,s,Y,e,m):
   return e == h(str(addP(mulP(G,s),mulP(Y,e))[0]) + m)
 
-def ecdsa_v(G,m,S,Y):
-  si = inv(S[0], r)
-  hh = h(m + str(S[1]))
-  u1 = (si * hh) % r
-  u2 = (si * S[1]) % r
-  return addP( mulP(G, u1), mulP(Y, u2) )[0] == S[1]
 
 x = 1234567
 while pow(x**3 + a*x + b, (prime - 1)/2, prime) != 1:
@@ -180,7 +149,7 @@ f = open(sys.argv[1], 'r')
 message = f.read()
 f.close()
 
-y = [readNumber('y0'), readNumber('y1')]
+y = [PublickeyX, PublickeyY]
 sig = [readNumber('s0'), readNumber('s1')]
 
 print "Public key: X: ", y[0] % prime
@@ -190,56 +159,4 @@ print "Sigature    y: ", sig[1]
 print ""
 print "The verification of signature ", verify(P, sig[0], y, sig[1], message)
 #print "The verification of ecdsa signature ", ecdsa_v(P,message,sig, y)
-
-def writeNumber(number, fnam):
-  f = open(fnam, 'wb')
-  n = number
-  while n > 0:
-    byte = n % 256
-    n = n / 256
-    f.write(chr(byte))
-  f.close()
-
-def wa(number, fnam):
-  f = open(fnam, 'a')
-  n = number
-  while n > 0:
-    byte = n % 256
-    n = n / 256
-    f.write(chr(byte))
-  f.close()
-
-def signSchnorr(G,m,x):
-  k = h(m + 'key value') # differs if m is different
-  R = mulP(G,k)
-  e = h(str(R[0]) + m)
-  return [(k - x*e) % r, e]
-
-hxx = h('password ABC123_0815')           # private key
-sig = signSchnorr(P, message, hxx)   # digital signature
-
-y = mulP(P, hxx)
-writeNumber(sig[0],'s0')
-writeNumber(sig[1],'s1')
-writeNumber(y[0],'y0')
-writeNumber(y[1],'y1')
-
- 
-lb = r
-cx = 0
-while lb > 0:
-  lb = lb/2
-  cx = cx + 1
-
-print "Bitlength ", cx
-print "\nChallenege: a = 0, b = p - 3, \n p = ", prime, "\n check prime    ", pow(7,prime-1,prime) == 1
-q = r
-print " check (p+1)/12 ", pow(7,q-1,q) == 1
-print " check order    ", mulP(P,q+1) == P
-print "\n\nPx ", P[0]
-print "Py ", P[1]
-
-Q = mulP(P, random.randint(2,prime-2) )
-print "\n\nQx ", Q[0]
-print "Qy ", Q[1]
 

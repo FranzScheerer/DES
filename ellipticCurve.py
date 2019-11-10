446647266b325bc24057a2846c8d4ef7c8d13774
Franz Scheerer <scheerer.software@gmail.com>
2019-11-10 11:39:54 +0100

    Super singular elliptic cure with Python 3
    knoppix@Microknoppix:~/DES-master$ python vFS.py V ellipticCurve.py 1HqiDbJjIMkLMKkPUlBUtTFENMtgBP720fmko/SRGWctU9MwY#gEMwC6SG3wJQmpO/2jF24vSmjh1nYM80Ylp8JIt9BgihcUS1YebA#dfNnB#s6NnJS0Fw30ekn2tSsYBoGZoJf3AdC29XIJ7oYsBvnTQAl3RZpYJmGb5L# 
    
     rabin signature - copyright Scheerer Software 2019 - all rights reserved
    
    
    First parameter is V (Verify)
    
    
    result of verification: True

--- ellipticCurve.py
+++ ellipticCurve.py
@@ -1,102 +1,63 @@
-'''
-E: Set of points with
-   y^2 = (x^3 - x) mod p 
-plus neutral element at infinity defines a smooth elliptic curve.
-p := (2^160 * 5 * 23) + 86427
-is a prime number.
-The number
-n4 := (p + 1) / 4
-is also a prime. 
-G is specially chosen point on the curve E, 
-such that n4 is the order of the subgroup of that point G.
-'''
-import sys, math, hashlib, random, time
+import random, math, getpass, sys
 
-def writeNumber(number, fnam):
-  f = open(fnam, 'wb')
-  n = number
-  while n > 0:
-    byte = n % 256
-    n = n / 256
-    f.write(chr(byte))
-  f.close()
+def update_spritz():
+    global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+    i_spritz = (i_spritz + w_spritz) % 256
+    j_spritz = s_spritz[(j_spritz + s_spritz[i_spritz]) % 256]
+    s_spritz[i_spritz], s_spritz[j_spritz] = s_spritz[j_spritz], s_spritz[i_spritz]
 
-# must be of the form 4k + 3 = 4(k+1) - 1
-# prime from NIST
-prime = 2**256 - 2**224 + 2**192 + 2**96 - 1
- 
-def bin2num(x):
-  res = 0
-  for c in x:
-    res = (res<<8) ^ ord(c)
-  return res
+def output_spritz():
+    global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+    update_spritz()
+    return s_spritz[j_spritz]
 
-def num2bin(x):
-  res = ''
-  while x > 0:
-    res = chr(x % 256) + res
-    x /= 256
-  return res
+def shuffle_spritz():
+    global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+    for v in range(256):
+        update_spritz()    
+    w_spritz = (w_spritz + 2) % 256
+    a_spritz = 0
+
+def absorb_nibble_spritz(x):
+    global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+    if a_spritz == 241:
+        shuffle_spritz()
+    s_spritz[a_spritz], s_spritz[240 + x] = s_spritz[240 + x], s_spritz[a_spritz]
+    a_spritz = a_spritz + 1
+
+def absorb_byte_spritz(b):
+    absorb_nibble_spritz(b % 16)
+    absorb_nibble_spritz(b // 16)
+
+def squeeze_spritz(out, outlen):
+    global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+    shuffle_spritz()
+    for v in range(outlen):
+        out.append(output_spritz())
 
 def gcd(a,b):
   while b > 0:
     a,b = b,a % b
   return a
 
+#
+# p and (p+1)//12 are primes
+#
 def nextPrime(p):
- while p % 8 != 3:
+ while p % 12 != 11:
    p = p + 1
  return nextPrime_odd(p)
 
 def nextPrime_odd(p):
-  m_ = 3*5*7*11*13*17*19*23*29
+  m_ =  5 * 7 * 11 * 13 * 17 * 19 * 23 * 29 * 31 * 37 * 41 * 43 * 47
   while True:
-    while gcd(p,m_) != 1:
-      p = p + 8 
-    q = (p+1)/4
-    if (pow(2,p-1,p) != 1 or pow(2,q-1,q) != 1):
-       p = p + 8
-       continue
-    if (pow(3,p-1,p) != 1 or pow(3,q-1,q) != 1):
-       p = p + 8
-       continue
-    if (pow(5,p-1,p) != 1 or pow(5,q-1,q) != 1):
-       p = p + 8
-       continue
-    if (pow(17,p-1,p) != 1 or pow(17,q-1,q) != 1):
-       p = p + 8
-       continue
-    break
-  return p
+    while gcd(p, m_) != 1 or gcd((p+1)//12, m_) != 1:
+      p = p + 12 
+    if (pow(7,p-1,p) != 1 or pow(7, (p+1)//12 - 1, (p+1)//12) != 1):
+      p = p + 12
+      continue
+    return p
 
-#prime = nextPrime(115 * 2**160)
-prime = 2**160 * 115 + 86427
-
-n4 = (prime + 1) / 4
-a = prime - 1
-
-def inv(b,m):
-  s = 0
-  t = 1
-  a = m
-  while b != 1:
-    q = a/b
-    aa = b
-    b = a % b
-    a = aa
-    ss = t
-    t = s - q*t
-    s = ss
-  if t < 0:
-    t = t + m
-  return t
-
-def E(m, key, prime):
-  return pow(m, key, prime)
-
-def D(c, key, prime):
-  key = inv(key, prime - 1)
-  return pow(c, key, prime)
 
 def hextxt2num(x):
   res = 0
@@ -105,90 +66,66 @@ def hextxt2num(x):
        res = (res<<4) + ord(c) - 48
     elif ord(c) <= ord('f') and ord(c) >= ord('a'):
        res = (res<<4) + ord(c) - 87
+    elif ord(c) <= ord('F') and ord(c) >= ord('A'):
+       res = (res<<4) + ord(c) - 55
   return res
 
-bhex = "5ac635d8 aa3a93e7 b3ebbd55 769886bc 651d06b0 cc53b0f6 3bce3c3e 27d2604b" 
-b = hextxt2num(bhex)
-#prime = nextPrime(2**500)
-b = 0
-def code2num(x):
-  res = 0
-  for c in x:
-     if ord(c) >= 48 and ord(c) < 58:
-       res = (res << 6) + ord(c) - 48
-     if ord(c) >= 65 and ord(c) < 91:
-       res = (res << 6) + ord(c) - 55
-     if ord(c) >= 97 and ord(c) < 123:
-       res = (res << 6) + ord(c) - 61
-     if c == '#': 
-       res = (res << 6) + 62
-     if c == '/': 
-       res = (res << 6) + 63
-  return res
+prime = nextPrime(12 * 2**131)
 
-def num2code(x):
-  res = ''
-  while x > 0:
-    y = x % 64
-    if y < 10:
-       res = chr( y + 48 ) + res
-    elif y < 36:
-       res = chr( y + 55 ) + res
-    elif y < 62:
-       res = chr( y + 61 ) + res 
-    elif y == 62:
-       res = '#' + res 
-    elif y == 63:
-       res = '/' + res 
-    x /= 64
-  return res
+print ("A prime greater than 12 times 2^131 \np = ", prime)
 
+a = 0
+b = prime - 3 # b = -3 modulo prime
+ 
+r = (prime + 1) // 12
+hsize = r
+ 
+def inv(b,a):
+  m = a
+  s = 0
+  t = 1
+  while b != 1:
+    q = a//b
+    a_tmp = b
+    b = a % b
+    a = a_tmp
+    s_tmp = t
+    t = s - q*t
+    s = s_tmp
+  if t < 0:
+    t = t + m
+  return t
 
 def h(x):
-  dx1 = hashlib.sha256(x).digest()
-  res = 0
-  for cx in (dx1):
-    res = (res<<8) ^ ord(cx)
-  return res % n4
+  global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+  i_spritz = j_spritz = a_spritz = 0
+  w_spritz = 1
+  s_spritz = []
+  for ix in range(256):
+    s_spritz.append(ix)
+  #
+  # absorb all bytes
+  #
+  for c in x:
+     absorb_byte_spritz(ord(c)) 
 
-def random256(m):
-  md = hashlib.sha256("***RANDOM-SEED_X***")
-  md.update('large key value for generation of random number')
-  md.update( m )
-  result = 0
-  largestr = md.digest()
-  for i in range(len(largestr)):
-      result = (result << 8) ^ ord(largestr[i])
-  return result
-
-def randomX(m):
-  md = hashlib.sha256("***RANDOM-SEED_X***")
-  md.update('large key value for generation of random number')
-  md.update( m )
-#  md.update( str(random.randint(0, 999999999999)) )
-  md.update( str(time.gmtime().tm_year + time.gmtime().tm_mday))
-  result = 0
-  largestr = md.digest()
-  for i in range(len(largestr)):
-      result = (result << 8) ^ ord(largestr[i])
-  return result
-
-def genP(x,a,b):
-   if pow(x**3 + a*x + b, (prime - 1)/2, prime) != 1:
-     x = prime - x
-   y = pow(x**3 + a*x + b, (prime + 1)/4, prime)
-   return [(x) % prime, (y) % prime]
+  res = []
+  squeeze_spritz(res, 32) # 8*32 = 256 bits
+  out = 0 
+  for bx in res:
+    out = (out<<8) + bx
+  return out % (hsize)
 
 def addP(P,Q):
   x1 = P[0]
   x2 = Q[0]
   y1 = P[1] 
   y2 = Q[1] 
-  if x2 > x1:
-    x2 = x2 - prime
   if x1 == x2:
      s = ((3*(x1**2) + a) * inv(2*y1, prime)) % prime
-  else:
+  else:  
+     if x1 < x2:
+        x1 = x1 + prime
      s = ((y1-y2) * inv(x1-x2, prime)) % prime
   xr = s**2 - x1 - x2
   yr = s * (x1-xr) - y1 
@@ -202,79 +139,72 @@ def mulP(P,n):
     n = (-1)*n 
   PP = resP
   while n > 0:
-     if (n & 1) != 0:   
+     if (n % 2 != 0):   
          if isFirst:
             resP = PP
             isFirst = False
          else:
             resP = addP(resP,PP)
-     PP = addP(PP, PP)
-     n = n >> 1 
+     PP = addP(PP, PP) 
+     n = n // 2
   return resP
 
-def signSchnorr(G,m,x):
-  k = randomX(m)
-  R = mulP(G,k)
-  e = h(str(R[0]) + m)
-  return [(k - x*e) % n4, e]
-
-def ecdsa(G,m,x):
-  k = randomX(m)
-  R = mulP(G,k)
-  hh = h(m+str(R[0]))
-  s = ( inv(k,n4) * (hh + R[0]*x) ) % n4
-  return [s, R[0]]
+def verify(G,s,Y,e,m):
+  return e == h(str(addP(mulP(G,s),mulP(Y,e))[0]) + m)
 
 def ecdsa_v(G,m,S,Y):
-  si = inv(S[0], n4)
+  si = inv(S[0], r)
   hh = h(m + str(S[1]))
-  u1 = (si * hh) % n4
-  u2 = (si * S[1]) % n4
+  u1 = (si * hh) % r
+  u2 = (si * S[1]) % r
   return addP( mulP(G, u1), mulP(Y, u2) )[0] == S[1]
 
+def signSchnorr(G,m,x):
+  k = h(m + 'key value') # differs if m is different
+  R = mulP(G,k)
+  e = h(str(R[0]) + m)
+  return [(k - x*e) % r, e]
 
-# x-value of the starting point  
-x = a - 17
-
-# The starting point which is added many times to itself 
-#cinv = inv(c, prime)
-cinv = 1
-P = genP(x,a,b)
-P = mulP(P,4)
-#Q = P = [2,2]
-#for xx in range(10):
-#  if mulP(P,xx+2) == P: 
-#    print "ERROR ", xx
-
-# mulP(P,n_) to test mulP(P,b_) is infty :)
-
-f = open(sys.argv[1],'r')
-message = f.read()
-f.close()
-
-x = 2*random256(sys.argv[1]) + 1
-y = mulP(P,x)
-
-#print "The public key for Schnorr's signature \n", y
-writeNumber(y[0],'y0')
-writeNumber(y[1],'y1')
-
-#sig = signSchnorr(P, message, x)
-sig = ecdsa(P, message, x)
-print "Verify: ", ecdsa_v(P,message,sig,y)
-
-writeNumber(sig[0],'s0')
-writeNumber(sig[1],'s1')
+#Generate the base point
+x = 1234567
+while pow(x**3+a*x+b, (prime-1)//2, prime) != 1:
+   x = x + 1
+y = pow(x**3+a*x+b, (prime+1)//4, prime)
+P = [x % prime, y % prime]
 
 
-print "test Schnoor ", h(str(addP(mulP(P,sig[0]),mulP(y,sig[1]))[0]) + message) == sig[1]
+P = mulP(P, 12)
+message = "Test message *******"
 
-print "Challenge: Find random multiplier of point P\n\n"
-print "Base point P: \n", P[0], "\n", P[1]
-P1 = mulP(P,random.randint(7,2**200))
-P2 = mulP(P,random.randint(77,2**201))
-P3 = mulP(P,random.randint(7777,2**202))
+privateKey = h('mypassword')
 
-print "\n\n Point 1\n", P1[0] , "\n" , P1[1]
-print "\n\n Point 2\n", P2[0] , "\n" , P2[1]
-print "\n\n Point 3\n", P3[0] , "\n" , P3[1]
+sig = signSchnorr(P, message, privateKey)
+
+publicKey = mulP(P, privateKey)
+
+#Verification
+R = addP(mulP(P, sig[0]), mulP(publicKey, sig[1]))
+check = h(str(R[0])+message) == sig[1]
+print("Result of verification ", check)
+
+ 
+lb = r
+cx = 0
+while lb > 0:
+  lb = lb//2
+  cx = cx + 1
+
+print ("Bitlength ", cx)
+print ("\nChallenege: a = 0, b = p - 3, \n p = ", prime, "\n check prime    ", pow(7,prime-1,prime) == 1)
+q = r
+print (" check (p+1)//12 ", pow(7,q-1,q) == 1)
+print (" check order    ", mulP(P,q+1) == P)
+print ("\n\nPx ", P[0])
+print ("Py ", P[1])
+
+# Multiply point P with random factor x. The result ist point Q.
+Q = mulP(P, random.randint(2,prime-2) )
+print ("Challenge: Find factor x such that x times P is Q\n\nQx ", Q[0])
+print ("Qy ", Q[1])
+
+

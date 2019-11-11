5043e235313e262582f23c1babd943d940da6eff
Franz Scheerer <scheerer.software@gmail.com>
2019-11-11 11:42:47 +0100

    The most simple elliptic curve: x^3 + 1 modulo large prime with (prime+1)/12 is prime, too
    knoppix@Microknoppix:~/DES-master$ python3 ec.py
    A prime greater than 12 times 2^131 
    p =  32667107224410092492483962313449748403011
    
    
     Read message to sign from file:  vFS.py
    The signature is:  [2712030271171094561770164539771629215502, 1197890670691698414451411528728939136433]
    
    Result of verification  True

--- ec.py
+++ ec.py
@@ -1,86 +1,116 @@
-import random, math, hashlib, sys
+import random, math, getpass, sys
+
+def update_spritz():
+    global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+    i_spritz = (i_spritz + w_spritz) % 256
+    j_spritz = s_spritz[(j_spritz + s_spritz[i_spritz]) % 256]
+    s_spritz[i_spritz], s_spritz[j_spritz] = s_spritz[j_spritz], s_spritz[i_spritz]
+
+def output_spritz():
+    global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+    update_spritz()
+    return s_spritz[j_spritz]
+
+def shuffle_spritz():
+    global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+    for v in range(256):
+        update_spritz()    
+    w_spritz = (w_spritz + 2) % 256
+    a_spritz = 0
+
+def absorb_nibble_spritz(x):
+    global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+    if a_spritz == 241:
+        shuffle_spritz()
+    s_spritz[a_spritz], s_spritz[240 + x] = s_spritz[240 + x], s_spritz[a_spritz]
+    a_spritz = a_spritz + 1
+
+def absorb_byte_spritz(b):
+    absorb_nibble_spritz(b % 16)
+    absorb_nibble_spritz(b // 16)
+
+def squeeze_spritz(out, outlen):
+    global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+    shuffle_spritz()
+    for v in range(outlen):
+        out.append(output_spritz())
 
 def gcd(a,b):
   while b > 0:
     a,b = b,a % b
   return a
 
+#
+# p and (p+1)//12 are primes
+#
 def nextPrime(p):
- while p % 24 != 19:
+ while p % 12 != 11:
    p = p + 1
  return nextPrime_odd(p)
 
 def nextPrime_odd(p):
   m_ =  5 * 7 * 11 * 13 * 17 * 19 * 23 * 29 * 31 * 37 * 41 * 43 * 47
   while True:
-    while gcd(p, m_) != 1 or gcd((p+1)/4, m_) != 1:
-      p = p + 24 
-    if (pow(7,p-1,p) != 1 or pow(7, (p+1)/4 - 1, (p+1)/4) != 1):
-      p = p + 24
+    while gcd(p, m_) != 1 or gcd((p+1)//12, m_) != 1:
+      p = p + 12 
+    if (pow(7,p-1,p) != 1 or pow(7, (p+1)//12 - 1, (p+1)//12) != 1):
+      p = p + 12
       continue
     return p
 
+#
+# (prime+1) divided by 12 ts prime
+#
+prime = nextPrime(12 * 2**131)
 
-def readNumber(fnam):
-  f = open(fnam, 'rb')
-  n = 0
-  snum = f.read()
-  for i in range(len(snum)):
-    n = (n << 8) ^ ord(snum[len(snum)-i-1])   
-  f.close()
-  return n
+print ("A prime greater than 12 times 2^131 \np = ", prime)
 
-def hextxt2num(x):
-  res = 0
-  for c in x:
-    if ord(c) < 58 and ord(c) >= 48:
-       res = (res<<4) + ord(c) - 48
-    elif ord(c) <= ord('f') and ord(c) >= ord('a'):
-       res = (res<<4) + ord(c) - 87
-    elif ord(c) <= ord('F') and ord(c) >= ord('A'):
-       res = (res<<4) + ord(c) - 55
-  return res
-
-#prime = (2**160 * 5 * 23) + 86427
-prime = 2**(256)-2**(224)+2**(192)+2**(96)-1
-a = prime - 3
-b = hextxt2num("5AC635D8 AA3A93E7 B3EBBD55 769886BC 651D06B0 CC53B0F6 3BCE3C3E 27D2604B")
-n4 = hextxt2num("FFFFFFFF 00000000 FFFFFFFF FFFFFFFF BCE6FAAD A7179E84 F3B9CAC2 FC632551")
-hsize = 2**100 + 7
- 
-def inv(b,m):
+def inv(b,a):
+  m = a
   s = 0
   t = 1
-  a = m
   while b != 1:
-    q = a/b
-    aa = b
+    q = a//b
+    a_tmp = b
     b = a % b
-    a = aa
-    ss = t
+    a = a_tmp
+    s_tmp = t
     t = s - q*t
-    s = ss
+    s = s_tmp
   if t < 0:
     t = t + m
   return t
 
 def h(x):
-  dx1 = hashlib.sha256(x).digest()
-  res = 0
-  for cx in (dx1):
-    res = (res<<8) ^ ord(cx)
-  return res % hsize
+  global a_spritz, i_spritz, j_spritz, w_spritz, s_spritz
+  i_spritz = j_spritz = a_spritz = 0
+  w_spritz = 1
+  s_spritz = []
+  for ix in range(256):
+    s_spritz.append(ix)
+  #
+  # absorb all bytes
+  #
+  for c in x:
+     absorb_byte_spritz(ord(c)) 
+
+  res = []
+  squeeze_spritz(res, 32) # 8*32 = 256 bits
+  out = 0 
+  for bx in res:
+    out = (out<<8) + bx
+  return out % (prime+1)//12
 
 def addP(P,Q):
   x1 = P[0]
   x2 = Q[0]
   y1 = P[1] 
   y2 = Q[1] 
-  while x1 < x2:
-     x1 = x1 + prime
   if x1 == x2:
-     s = ((3*(x1**2) + a + prime) * inv(2*y1, prime)) % prime
+     s = (3*(x1**2) * inv(2*y1, prime)) % prime
   else:  
+     if x1 < x2:
+        x1 = x1 + prime
      s = ((y1-y2) * inv(x1-x2, prime)) % prime
   xr = s**2 - x1 - x2
   yr = s * (x1-xr) - y1 
@@ -89,9 +119,6 @@ def addP(P,Q):
 def mulP(P,n):
   isFirst = True
   resP = P
-  if n < 0:
-    resP[1] = prime - resP[1]
-    n = (-1)*n 
   PP = resP
   while n > 0:
      if (n % 2 != 0):   
@@ -101,75 +128,42 @@ def mulP(P,n):
          else:
             resP = addP(resP,PP)
      PP = addP(PP, PP) 
-     n = n / 2
+     n = n // 2
   return resP
 
-def verify(G,s,Y,e,m):
-  return e == h(str(addP(mulP(G,s),mulP(Y,e))[0]) + m)
-
-def ecdsa_v(G,m,S,Y):
-  si = inv(S[0], n4)
-  hh = h(m + str(S[1]))
-  u1 = (si * hh) % n4
-  u2 = (si * S[1]) % n4
-  return addP( mulP(G, u1), mulP(Y, u2) )[0] == S[1]
-
-x = 1
-while pow(x**3 + a*x + b, (prime - 1)/2, prime) != 1:
-   x = x + 1
-y = pow(x**3 + a*x + b, (prime + 1)/4, prime)
-P = [x % prime, y % prime]
-P = mulP(P,4)
-
-f = open(sys.argv[1], 'r')
-message = f.read()
-f.close()
-
-y = [readNumber('y0'), readNumber('y1')]
-sig = [readNumber('s0'), readNumber('s1')]
-
-print "Public key: X: ", y[0] % prime
-print "Public key: Y: ", y[1] % prime
-print "Sigature    X: ", sig[0]
-print "Sigature    y: ", sig[1]
-print ""
-print "The verification of signature ", verify(P, sig[0], y, sig[1], message)
-#print "The verification of ecdsa signature ", ecdsa_v(P,message,sig, y)
-
-def writeNumber(number, fnam):
-  f = open(fnam, 'wb')
-  n = number
-  while n > 0:
-    byte = n % 256
-    n = n / 256
-    f.write(chr(byte))
-  f.close()
-
-def signSchnorr(G,m,x):
-  k = h(m + 'kk1')
+def signSchnorr(G, m, x):
+  # k is different if m is different
+  k = h(m + 'key value') 
   R = mulP(G,k)
   e = h(str(R[0]) + m)
-  return [(k - x*e) % n4, e]
+  return [(k - x*e) % ((prime+1)//12), e]
 
-def ecdsa(G,m,x):
-  k = h(m + 'ecdsa')
-  R = mulP(G,k)
-  hh = h(m+str(R[0]))
-  s = ( inv(k,n4) * (hh + R[0]*x) ) % n4
-  return [s, R[0]]
+#Generate the base point
+x = 1234567
+while pow(x**3 + 1, (prime-1)//2, prime) != 1:
+   x = x + 1
+y = pow(x**3 + 1, (prime+1)//4, prime)
+P = [x % prime, y % prime]
 
-hxx = h('kk1_' + str(777*random.random()))
-sig = signSchnorr(P, message, hxx)
-y = mulP(P, hxx)
-dsa = ecdsa(P,message,hxx)
-print "The verification of ecdsa signature  ", ecdsa_v(P,message,dsa,y)
 
-writeNumber(sig[0],'s0')
-writeNumber(sig[1],'s1')
-writeNumber(y[0],'y0')
-writeNumber(y[1],'y1')
-print "\n TEST TEST TEST "
-print "prime       ", pow(7,prime-1,prime) == 1 
-print "prime order ", pow(7,n4-1,n4) == 1 
-print "order       ", P == mulP(P,n4+1) 
+P = mulP(P, 12)
 
+fo = open('vFS.py','r')
+print("\n\n Read message to sign from file: ", fo.name)
+message = fo.read()
+fo.close()
+
+privateKey = h('mypassword')
+
+sig = signSchnorr(P, message, privateKey)
+print("The signature is: ",sig)
+publicKey = mulP(P, privateKey)
+
+#Verification
+#sig = [217386284724654099298610386906895450235, 1593176211314688851384299242063611055952]
+R = addP(mulP(P, sig[0]), mulP(publicKey, sig[1]))
+check = h(str(R[0])+message) == sig[1]
+print("\nResult of verification ", check)
+print()
+
+ 

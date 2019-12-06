92ab72992ffd5d58dc6cce5bb522575b7f07b332
Franz Scheerer <scheerer.software@gmail.com>
2019-12-06 13:59:30 +0100

    ECC only - we use standard hash SHA-256 in Python 3 now
    knoppix@Microknoppix:~/DES-master$ python3 curve.py curve.py
    A prime greater than 2^131 
    p =  50512109465528789995377072747180563842243
    The base point is: 
    x:  40617143159824489217478505568526166409076
    y:  13653137689015501035936468357752821555591
    The public key is the point: 
    x:  50044575718653307843747419070208817674773
    y:  10020898080861333362084267181808827266303
    The signature is: 
    s:  7149653545483376373290128838990545257110
    e:  1143906177916153835105563282972470869967
    
    Result of verification  True
    The quick brown fox jumps over the lazy dog:
     h =  d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592

---
+++ curve.py
@@ -0,0 +1,161 @@
+'''
+  Schnorr Signature 
+  Secure
+  Simple curve: y^2 = x^3 - x (modulo prime)
+  Hash SHA-256 
+  PYthon 3
+
+  Copyright (c) Scheerer Software 2019 - all rights reserved 
+''' 
+import sys, hashlib
+
+def writeNumber(number, fnam):
+  f = open(fnam, 'w')
+  f.write(str(number))
+  f.close()
+
+def readNumber(fnam):
+  f = open(fnam, 'r')
+  n = 0
+  snum = f.read()
+  for c in snum:
+    n = (n * 10) + ord(c) - 48   
+  f.close()
+  return n
+
+import hashlib
+def h(x):
+  y = x.encode(encoding = 'UTF-8',errors = 'strict')
+  out = 0 
+  for bx in hashlib.sha256(y).digest():
+    out = (out<<8) + bx
+  return out
+
+def num2hextxt(x):
+  res = ''
+  h__ = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+  while x > 0:
+    res = h__[x % 16] + res
+    x >>= 4
+  return res
+
+def hF(x):
+  f = open(x, "rb")
+  out = 0
+  for bx in f.read():
+    out = (out<<8) + bx
+  f.close()
+  return num2hextxt(out)
+
+#
+# p and (p+1) divied by 4 are primes
+# There are (p+1) points on the curve.
+# https://math.stackexchange.com/questions/319742/order-of-elliptic-curve-y2-x3-x-defined-over-f-p-where-p-equiv-3-m
+#
+def gcd(a,b):
+  while b > 0:
+    a,b = b,a % b
+  return a
+
+def nextPrime(p):
+ while p % 12 != 7:
+   p = p + 1
+ return nextPrime_(p)
+
+def nextPrime_(p):
+  m_ =  5 * 7 * 11 * 13 * 17 * 19 * 23 * 29 * 31 * 37 * 41 * 43 * 47
+  while True:
+    while gcd(p, m_) != 1 or gcd((p+1)>>2, m_) != 1:
+      p = p + 12 
+    if (pow(7,p-1,p) != 1 or pow(7, ((p+1)>>2) - 1, (p+1)>>2) != 1):
+      p = p + 12
+      continue
+    return p
+
+#
+# (prime+1) divided by 4 ts prime
+#
+maxx = 131 * 2**131
+prime = nextPrime( h('Franz Scheerer') % maxx )
+print ("A prime greater than 2^131 \np = ", prime)
+
+#
+# Add point P and Q on the curve
+#
+def addP(P,Q):
+  global prime
+  x1 = P[0]
+  x2 = Q[0]
+  y1 = P[1] 
+  y2 = Q[1] 
+  if x1 == x2:
+     s = ((3*x1*x1 - 1) * pow(2*y1, prime-2, prime)) % prime
+  else:  
+     if x1 < x2:
+        x1 = x1 + prime
+     s = ((y1-y2) * pow(x1-x2, prime-2, prime)) % prime
+  xr =  (s*s) - x1 - x2
+  yr = s * (x1-xr) - y1 
+  return [xr % prime, yr % prime]
+
+#
+# P + P + P + P + P + P --- n times
+# 
+def mulP(P,n):
+  global prime
+  resP = 'ZERO'
+  PP = P
+  while n != 0:
+     if (n % 2 != 0):   
+         if resP == 'ZERO':
+            resP = PP
+         else:
+            resP = addP(resP,PP)
+     PP = addP(PP, PP) 
+     n >>= 1 
+  return resP
+
+def signSchnorr(G, m, x):
+  global prime
+  # k is different if message m is different
+  k = h(m + 'key value') 
+  R = mulP(G,k)
+  e = h(str(R[0]) + m) % ((prime+1)>>2)
+  return [(k - x*e) % ((prime+1)>>2), e]
+
+#Generate the base point
+x = 1234567
+if pow(x**3 - x, (prime-1)>>1, prime) != 1:
+   x = prime - x 
+y = pow( x**3 - x, (prime+1)>>2, prime)
+P = [ x % prime, y % prime ]
+
+
+P = mulP(P, 4)
+
+f = open(sys.argv[1],"rb")
+message = hashlib.sha256(f.read()).hexdigest()
+
+privateKey = h( 'passwordX' ) # some random number
+print("The base point is: ")
+print("x: ",P[0])
+print("y: ",P[1])
+
+publicKey = mulP(P, privateKey)
+
+print("The public key is the point: ")
+print("x: ",publicKey[0])
+print("y: ",publicKey[1])
+
+sig = signSchnorr(P, message, privateKey)
+print("The signature is: ")
+print("s: ",sig[0])
+print("e: ",sig[1])
+
+#Verification
+R = addP(mulP(P, sig[0]), mulP(publicKey, sig[1]))
+check = h(str(R[0])+message) % ((prime+1)>>2) == sig[1]
+print("\nResult of verification ", check)
+
+hash = h("The quick brown fox jumps over the lazy dog")
+print ("The quick brown fox jumps over the lazy dog:\n h = ", num2hextxt(hash))
